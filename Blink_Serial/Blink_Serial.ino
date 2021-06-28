40d401de2112ec6be46a716d4f9b63df256fbd04
morbihan <31599890+morbihan@users.noreply.github.com>
2021-06-28 21:28:22 +0200

    Update Blink_Serial.ino

--- Blink_Serial/Blink_Serial.ino
+++ Blink_Serial/Blink_Serial.ino
@@ -28,7 +28,7 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize both serial ports:
+  // initialize both port.
   Serial.begin(9600);
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
@@ -36,7 +36,7 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on by making the voltage LOW
   Serial.println("LED is on.");
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
